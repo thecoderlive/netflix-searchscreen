561caed4a6d3e6774e1a0343756d4f4b5489c209
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-02 13:57:49 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,35 @@
+export const item = {
+    "id": "5c0059da-5f00-4360-a719-2647160a9aaf",
+    "home_feed": [
+        {
+            "id": "e792d603-8e9e-48c3-8673-8ccb746ef53e",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/219.png",
+            "video_name": "order",
+            "play_pause": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png"
+        },
+        {
+            "id": "fdea1738-0ab8-4ca9-a6e5-85d39f76b8c3",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/339.png",
+            "video_name": "name",
+            "play_pause": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+        },
+        {
+            "id": "7029b8cf-e550-4803-9a30-45dd985ef7ac",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png",
+            "video_name": "story",
+            "play_pause": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+        },
+        {
+            "id": "b058361f-ee7a-4584-97bf-c9a08fbf7b37",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png",
+            "video_name": "test",
+            "play_pause": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+        },
+        {
+            "id": "b803f186-04b0-4ea1-84dc-35d9a2c1215d",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png",
+            "video_name": "sister",
+            "play_pause": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+        }
+    ]
+}
